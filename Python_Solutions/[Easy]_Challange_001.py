6edd59a6b23f876a828fdcc2227b19af25bf2a62
Michael Arnold <valestrum@hotmail.com>
2014-06-17 03:35:45 -0400

    Created solution to [easy]challange#1 using Python

---
+++ Python_Solutions/[Easy]_Challange_001.py
@@ -0,0 +1,19 @@
+# Objective:
+# Create a program that will ask the users name, age, and Reddit username. 
+# Have it tell them the information back, in the format:
+# "Your name is (blank), you are (blank) years old, and your username is (blank)"
+# for extra credit, have the program log this information in a file to be accessed later.
+# URL : https://pay.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
+
+def main():
+        name = raw_input("Please enter your name: ")
+        age = raw_input("Please enter your age: ")
+        redditUsername = raw_input("Please enter your Reddit username: ")
+        print("Your name is %s, you are %s years old, and your Reddit username is %s." % (name,age,redditUsername))
+
+        # Store in a file
+        out = open("easyChallange001_Storage.txt", 'a')
+        storedInfo = "%s, %s, %s\n" % (name,age,redditUsername)
+        out.write(storedInfo)
+        out.close()
+main()
