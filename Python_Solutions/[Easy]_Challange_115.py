83fd2cfd59e651b66302f47501820d341705309f
Michael Arnold <valestrum@hotmail.com>
2014-07-14 22:10:27 -0400

    Create [Easy]_Challange_115.py

---
+++ Python_Solutions/[Easy]_Challange_115.py
@@ -0,0 +1,25 @@
+import random
+
+def main():
+    print("Welcome to guess-that-number game!\n"
+          +"If at any point you would like to quit, please type 'exit'.\n\n"
+          +"I have thought of a number, 1-100, see if you can guess it!")
+    number = random.randrange(1,101)
+    guess = 0
+
+    while(guess != number):
+        guess = raw_input("Guess a number: ")
+        guess = guess.lower()
+        if(guess == "exit"):
+            exit()
+
+        if(int(guess) == number):
+            print("You guessed it! You win!")
+            exit()
+        elif(int(guess) > number):
+            print("Your number is too high!")
+        elif(int(guess) < number):
+            print("Your number is too low!")
+        else:
+            print("Error.")        
+main()
