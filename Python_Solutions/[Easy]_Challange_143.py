cf126340e346aed0120fde26830fff17d6e6d72c
Michael Arnold <valestrum@hotmail.com>
2014-07-20 23:30:25 -0400

    Added two way conversion for Braille translator

--- Python_Solutions/[Easy]_Challange_143.py
+++ Python_Solutions/[Easy]_Challange_143.py
@@ -29,6 +29,20 @@ braille_dictionary = {
 'z': 'O..OOO'
 }
 
+def convert_to_english_from_braille(input):
+    reverse_dictionary = dict ((a,b) for b, a in braille_dictionary.items())
+    message = ''
+    print "\nYour messaged in English is..\n"
+    fixed_input = input
+    fixed_input = fixed_input.upper()
+    words = []
+    for start in range(0, len(fixed_input), 6):
+        words.append(fixed_input[start:(start + 6)])
+
+    for word in words:
+        message += "%s" % (reverse_dictionary[word])
+    print message
+
 def convert_to_braille_from_english(input):
     message = ''
     print "\nYour message in Binary is..\n"
@@ -47,9 +61,29 @@ def restart():
         print("Invalid entry, please try again.")
         restart()
 
+def menu():
+    phrase1 = "Type what you would like to convert into "
+    print("\n- - - - - - - -- PROGRAM MENU -- - - - - - - -\n"
+          "Please choose a desired conversion: \n"
+          "1 ) Braille to English\n"
+          "2 ) English to Braille\n"
+          "3 ) No conversion, exit program.")
+    user_choice = input("\n")
+    
+    if(user_choice == 1):
+        user_input = raw_input(str(phrase1) + "English\n::\t")
+        convert_to_english_from_braille(user_input)
+    elif(user_choice == 2):
+        user_input = raw_input(str(phrase1) + "Braille\n::\t")
+        convert_to_braille_from_english(user_input)
+    elif(user_choice == 3):
+        exit()
+    else:
+        print("Invalid selection. Please try again.")
+        main()
+    
+
 def main():
-    print("Type what you would like to convert into English\n")
-    user_input = raw_input("\n")
-    convert_to_braille_from_english(user_input)
+    menu()
     restart()
 main()
