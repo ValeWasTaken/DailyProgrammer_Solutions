aa4f34202f2eda71ea28c3ad6a7f3f86040766ce
Michael Arnold <valestrum@hotmail.com>
2014-07-31 20:38:38 -0400

    Create [Easy]_Challange_008.java

---
+++ Java_Solutions/[Easy]_Challange_008.java
@@ -0,0 +1,26 @@
+public class [Easy]_Challange_008 {
+	public static void main (String[]args)
+	{
+		int beerNum = 99;
+		String word = "bottles";
+
+		while (beerNum > 0)
+		{
+			if(beerNum == 1)
+			{word = "bottle";} // Change wording if last bottle.
+
+      //All printed on one line for extra credit.
+			System.out.print(beerNum + " " + word + " of beer on the wall! ");
+			System.out.print(beerNum + " " + word + " of beer! ");
+			System.out.print("Take one down, ");
+			System.out.print("pass it around, ");
+			beerNum--;
+		}
+		if(beerNum == 0)
+		{
+			System.out.println("No more bottles of beer on the wall");
+		}
+		else
+		{System.out.println("Error.");}
+	}
+}
