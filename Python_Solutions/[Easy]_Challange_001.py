865c507f064dbaec1c9b85ecef44add11b1f1ce9
Michael Arnold <valestrum@hotmail.com>
2014-09-02 19:56:14 -0400

    Removed useless comments

--- Python_Solutions/[Easy]_Challange_001.py
+++ Python_Solutions/[Easy]_Challange_001.py
@@ -1,9 +1,3 @@
-# Objective:
-# Create a program that will ask the users name, age, and Reddit username. 
-# Have it tell them the information back, in the format:
-# "Your name is (blank), you are (blank) years old, and your username is (blank)"
-# For extra credit, have the program log this information in a file to be accessed later.
-
 def main():
         name = raw_input("Please enter your name: ")
         age = raw_input("Please enter your age: ")
