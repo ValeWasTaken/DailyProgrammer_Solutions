ef474a458f4bd15b150b161bcdeda53c2e172114
Michael Arnold <valestrum@hotmail.com>
2014-09-02 19:52:54 -0400

    Removed useless comments

--- Python_Solutions/[Easy]_Challange_128.py
+++ Python_Solutions/[Easy]_Challange_128.py
@@ -1,9 +1,3 @@
-# Objective:
-# Take a number and find the sum of its digits.
-# Ex: 123456 is 1+2+3+4+5+6
-# Then find the sum of that number, and so forth, until you remain with one digit.
-#
-
 def main():
     sum = 0
     num = input("Enter a number: ")
