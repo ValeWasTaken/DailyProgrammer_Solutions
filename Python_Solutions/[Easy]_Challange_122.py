1d710993317d74f344f33168a136d8b0d7f6720b
Michael Arnold <valestrum@hotmail.com>
2014-09-02 19:53:17 -0400

    Removed useless comments

--- Python_Solutions/[Easy]_Challange_122.py
+++ Python_Solutions/[Easy]_Challange_122.py
@@ -1,7 +1,3 @@
-# Objective:
-# Find the digital root of a number.
-# Digital root example: 31337 -> 3+1+3+3+7 = 17 -> 1+7 = 8, the digital root.
-
 def main():
     num = input("Enter number to find digital root of: ")
     while(num > 9):
