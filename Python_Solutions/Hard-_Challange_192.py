2f1ad88645106b8ef5eb5a540e6efe72146bf55e
Michael Arnold <valestrum@hotmail.com>
2014-12-21 22:19:05 -0500

    Create Hard-_Challange_192.py

---
+++ Python_Solutions/Hard-_Challange_192.py
@@ -0,0 +1,14 @@
+#Counts the amount of $7.99 "Three book bundles" listed on thriftbooks.com
+
+import urllib
+from bs4 import BeautifulSoup
+
+def bundleCounter():
+        totalBundles = 0
+        url = 'http://www.thriftbooks.com/deals.aspx?featureid=402'
+        soup = BeautifulSoup(urllib.urlopen(url).read())
+
+        for bundle in soup.find_all("div", class_="bundle-item"):
+                totalBundles += 1
+        print("Total amount of bundles listed: "+str(totalBundles))
+bundleCounter()
