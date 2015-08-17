85ffaa322e9803c72f4f6b2dbb65a1310cacdf43
Michael Arnold <valestrum@hotmail.com>
2015-08-16 21:24:46 -0400

    Fixed spacing

--- Python_Solutions/[Easy]_Challange_004.py
+++ Python_Solutions/[Easy]_Challange_004.py
@@ -1,15 +1,15 @@
 from random import randint
 
 def main():
-        numOfPasswords = input("How many passwords would you like to generate? ")
-        passLength = input("How long would you like your password(s) to be? ")
-        selection = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    numOfPasswords = input("How many passwords would you like to generate? ")
+    passLength = input("How long would you like your password(s) to be? ")
+    selection = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
+    result = ''
+    for a in range(numOfPasswords):
+        for b in range(passLength):
+            place = randint(0, len(selection) -1)
+            result += selection[place]
+        print(result)
         result = ''
-        for a in range(numOfPasswords):
-                for b in range(passLength):
-                        place = randint(0, len(selection) -1)
-                        result += selection[place]
-                print(result)
-                result = ''
 main()
