f15ff59bc75b0718cced289708f3e3a69fd673c8
Michael Arnold <valestrum@hotmail.com>
2015-08-31 23:12:16 -0400

    Slightly optimized solution

--- Python_Solutions/[Hard]_Challenge_229.py
+++ Python_Solutions/[Hard]_Challenge_229.py
@@ -1,8 +1,6 @@
 def challenge_229(power):
-    sum = 0
     for x in range(0, 10**power, 7):
         if int(str(x)[::-1]) % 7 == 0:
-            sum += x
-    print(sum)
-    
-challenge_229(3)
+            yield x
+            
+print(sum(challenge_229(3)))
